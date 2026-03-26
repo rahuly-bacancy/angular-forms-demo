63c2fff7e1585bc048c3b9c1a2a6a13f49e02153
Rahul Yadav <rahul.yadav@bacancy.com>
2026-03-26 10:33:56 +0530

    Initial setup

--- src/app/features/user-form/user-form.component.ts
+++ src/app/features/user-form/user-form.component.ts
@@ -1,20 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-// This interface defines the shape of our form model.
-interface UserFormData {
-  fullName: string;
-  email: string;
-  password: string;
-  gender: string;
-  skills: {
-    angular: boolean;
-    react: boolean;
-    node: boolean;
-  };
-  country: string;
-  bio: string;
-}
 
 @Component({
   selector: 'app-user-form',
@@ -24,20 +10,6 @@ interface UserFormData {
   styleUrl: './user-form.component.scss',
 })
 export class UserFormComponent {
-  // This object stores the form data used by ngModel bindings.
-  userFormData: UserFormData = {
-    fullName: '',
-    email: '',
-    password: '',
-    gender: '',
-    skills: {
-      angular: false,
-      react: false,
-      node: false,
-    },
-    country: '',
-    bio: '',
-  };
 
   // These arrays keep repeated values out of the HTML.
   genderOptions = ['Male', 'Female'];

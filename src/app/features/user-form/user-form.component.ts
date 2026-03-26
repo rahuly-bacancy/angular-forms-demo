d2694ebcfcc8e7a3e1b6c39a38349b9720736020
Rahul Yadav <rahul.yadav@bacancy.com>
2026-03-26 11:43:36 +0530

    Demo pushed

--- src/app/features/user-form/user-form.component.ts
+++ src/app/features/user-form/user-form.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss',
 })
@@ -14,9 +15,34 @@ export class UserFormComponent {
   // These arrays keep repeated values out of the HTML.
   genderOptions = ['Male', 'Female'];
   countryOptions = ['India', 'United States', 'Canada'];
-
+  fullName = 'Rahul Kumar';
+  @ViewChild('userForm') userForm!: NgForm;
   // This method runs when the user submits the form.
-  onSubmit(): void {
-    
+  onSubmit(form: NgForm): void {
+    console.log(this.userForm);
+    this.userForm.reset();
   }
+  patchValue(): void {
+    this.userForm.form.patchValue({
+      userInfo: {
+        fullName: 'Rahul Kumar',
+        email: 'rahul.kumar@example.com'
+    }
+    });
+  }
+  setValue(): void {
+    this.userForm.form.setValue({
+      userInfo: {
+        fullName: 'Rahul Kumar Yadav',
+        email: 'rahul.kumar@example.com',
+        password: '123',
+      },
+      gender: '',
+      country: '',
+      bio: '',
+      rememberMe: false
+    });
+  }
+
+
 }
